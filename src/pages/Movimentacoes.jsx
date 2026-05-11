353d9105f1996bdef3f9fb63e18b5fe7ddb0385d
Cauã <cauammta141003@gmail.com>
2026-05-11 13:40:40 -0300

    Fechei Issue 2 Configurar rotas com React Router #2 Open

---
+++ src/pages/Movimentacoes.jsx
@@ -0,0 +1,10 @@
+const Movimentacoes = () => {
+  return (
+    <div>
+      <h1>Movimentações de Estoque</h1>
+      <p>Registro de entradas e saídas de produtos</p>
+    </div>
+  );
+};
+
+export default Movimentacoes;
